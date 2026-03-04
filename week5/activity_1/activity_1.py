5ad96dadf8f48e79cab6f79aa7b39bcd8b502530
ANDERA <ANDERA@L306-07>
2026-03-04 16:27:44 +0800

    end of day wk5

--- week5/activity_1/activity_1.py
+++ week5/activity_1/activity_1.py
@@ -1,15 +1,25 @@
 def main():
     # TODO Define 3 helper functions for text-to-bytes conversion, bytes-to-text conversion, and byte reversal
+    def text_to_bytes(string):
+        return string.encode('utf-8')
+    def bytes_to_text(bytes):
+        return bytes.decode('utf-8')
+    def integer_to_bytes(integer):
+        return (integer).to_bytes(length=1, byteorder='big')
+    def bytes_to_integer(bytes):
+        return int.from_bytes(bytes, byteorder='big')
 
 
     # Main program logic
     # TODO Open the binary file for reading and create output text and bytes files for writing using the context manager
     try:
-        
+        with open('activity_1/data.bin','rb', encoding="utf-8") as file, \
+             open('converted_text.txt', 'w', encoding="utf-8") as text_output,  \
+             open('reversed_bytes.bin.', 'wb', encoding="utf-8") as bytes_output:
                 # Iterate through each line in the binary file
                 for line in file:
                     # TODO Decode the line to Unicode string and remove leading/trailing whitespaces
-
+                    line = bytes_to_text(line).strip()
                     # Check if the line starts with "TEXT:"
                     if line.startswith("TEXT:"):
                         # Extract text content, convert to uppercase, and write to text file
@@ -18,12 +28,12 @@ def main():
                     
                     # Check if the line starts with "BYTES:"
                     elif line.startswith("BYTES:"):
-                        pass
+                        print(line)
 
                         # TODO Extract the string and encode to hexadecimal
-       
+                        encoded_hex_data = line [6:].encode().hex()
                         # TODO Extract byte content, convert to bytes object(using fromhex()), 
-     
+                        bytes_data = bytes.fromhex(encoded_hex_data)
                         # Using your helper functions 
                         # TODO 1. reverse bytes, and write to bytes file
 
--- week5/activity_2/activity_2.py
+++ week5/activity_2/activity_2.py
@@ -1,21 +1,26 @@
 def main():
-    pass
+
     # File Navigation
     with open('data_v2.bin', 'rb') as file:
         # Navigate to the 5th byte position
-        
+        print(file)
+        file.seek(6)
+        print(file.read(6))
         # Read and print the next 4 bytes from the current position
-
+        file.seek(4)
         # Move the file pointer to the beginning of the file
-
+        file.seek(0)
         # Read and print the first 100 pixels from the file
+        print(file.read(100))
 
         # Print the current file pointer position
-
+        print(f"the current position is {file.tell()}")
         # Use the find() method to search for the string "ABC" in the file
-
+        print(f"ABC is at position {file.read().find(b'D5 CE C7')}")
         # Get the position of the first pixel jump 10 and get the next pixel
-
+        pixel = file.read(3)
+        print(pixel)
+        print (pixel*10)
         # Print them out
 
         # Re-open the data.bin file in binary write-append mode
